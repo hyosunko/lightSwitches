27b9e5982dedebddf57c7d7938321722f7df8067
hyosunko <hyosunko@gmail.com>
2018-12-17 11:33:10 -0800

    modification for using local background picktures by uncommenting codes.

--- src/Light.js
+++ src/Light.js
@@ -11,40 +11,44 @@ class Light extends Component{
     this.state = {
       isOn: false,
       light: "off",
-      // img: "./lightOff.png"
+      // in case of using background pictures
+      // img: "/images/lightOff.png"
       // img: "http://www.myiconfinder.com/uploads/iconsets/256-256-9b952124a7973cedd363015231bfe197.png"
     }
     }
 
     // switch status changes by onClick on switch.  
     handleChange = () =>{
-    this.setState({isOn:!this.state.isOn});
-    this.setState({light:this.state.isOn?"off":"on"});
-    // this.setState({img:this.state.isOn?"./lihgtOn.png":"./lightOff.png"})
-    // this.setState({img:this.state.isOn?"http://www.myiconfinder.com/uploads/iconsets/256-256-9b952124a7973cedd363015231bfe197.png":"http://www.myiconfinder.com/uploads/iconsets/256-256-5c45fea7601361971f06202d034760dc.png"})
+		this.setState({isOn:!this.state.isOn});
+	    this.setState({light:this.state.isOn?"off":"on"});
+		// in case of using background pictures
+	    // this.setState({img:this.state.isOn?"/images/lightOff.png":"/images/lightOn.png"})
+	    // this.setState({img:this.state.isOn?"http://www.myiconfinder.com/uploads/iconsets/256-256-9b952124a7973cedd363015231bfe197.png":"http://www.myiconfinder.com/uploads/iconsets/256-256-5c45fea7601361971f06202d034760dc.png"})
     }
 
     render () {
-          // var sectionStyle ={
-          //     width: '100px',
-          //     height: '100px',
-          //     textAlign: 'center',
-          //     userSelect: 'none',
-          //     backgroundImage: `url(${this.state.img})`,
-          //     backgroundRepeat:'no-repeat',
-          //     backgroundSize: '100px 100px',
-          //     margin: 'auto',
-          //     // border: '3px solid'
-          // }
+		  // in case of using background pictures
+	      // var sectionStyle ={
+	      //     width: '100px',
+	      //     height: '100px',
+	      //     textAlign: 'center',
+	      //     userSelect: 'none',
+	      //     backgroundImage: `url(${this.state.img})`,
+	      //     backgroundRepeat:'no-repeat',
+	      //     backgroundSize: '100px 100px',
+	      //     margin: 'auto',
+	      //     // border: '3px solid'
+	      // }
 
         return(
             <div className="box-container">
-              {/*<div style={sectionStyle}  className="light-bulb"></div>*/}
 
               {/* light, checkbox input is controlled by css */}
+              {/*<div style={sectionStyle} className="light-container">*/}
               <div className="light-container">
                   <label>
 		              {/* light, checkbox checked status change by Onclick */}
+                      {/*<input ref="light" checked={ this.state.isOn } onChange={ this.handleChange} type="checkbox"/> */}
                       <input ref="light" checked={ this.state.isOn } onChange={ this.handleChange } className="light" type="checkbox" />
                       <div>
                       </div>
@@ -58,7 +62,7 @@ class Light extends Component{
                       <input ref="switch" checked={ this.state.isOn } onChange={ this.handleChange } className="switch" type="checkbox" />
                       <div>
                           <div>
-				            {/* switch, on/off charactr diplay */}
+				            {/* switch, on/off character diplay */}
                             {this.state.light}
                           </div>
                       </div>
