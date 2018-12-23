f3b40bcceb16c29fd063ab27defc5545bd45651c
hyosunko <hyosunko@gmail.com>
2018-12-23 07:36:21 -0800

    Updates

---
+++ precache-manifest.3c0d227d948f94a47263b350b4f4e058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c544f6d1d9053c4a611f",
+    "url": "/lightSwitches/static/js/runtime~main.c544f6d1.js"
+  },
+  {
+    "revision": "962883391111cd9ea908",
+    "url": "/lightSwitches/static/js/main.96288339.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/lightSwitches/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "962883391111cd9ea908",
+    "url": "/lightSwitches/static/css/main.0c83b9d0.chunk.css"
+  },
+  {
+    "revision": "8b52c51bb88eea07bba846a1a1a7be07",
+    "url": "/lightSwitches/index.html"
+  }
+];
